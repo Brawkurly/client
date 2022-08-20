56a1d4c2674856b7c3aadfc1ec5de55864269f10
지현 <57143818+spy03128@users.noreply.github.com>
2022-08-21 00:29:22 +0900

    Revert "[FEAT] Client 페이지 홈화면 디자인 (#4) (#7)" (#8)
    This reverts commit ef0264824f2211b41bfd214da1d11809f800648a.

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,18 +1,8 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navigation from "./components/header/Navigation/Navigation";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <h1>customer page</h1>
+    </div>
   );
 }
 
