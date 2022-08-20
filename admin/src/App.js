5cf57b2bb99885f517ece529c252abec224abed5
heogeon0 <heo3793@naver.com>
2022-08-20 20:15:18 +0900

    [feat] Admin페이지 Layout 배치 (#3)

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { Title } from "./items/items";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: ${(props) => props.theme.DarkBlue};
+
+  .grid {
+    display: grid;
+    grid-template-columns: 2fr 2.5fr 2fr;
+    grid-template-rows: repeat(12, 1fr);
+    grid-gap: 10px 10px;
+
+    height: 98vh;
+    padding: 20px;
+
+    background-color: ${(props) => props.theme.MidBlue};
+
+    .today_sell {
+      grid-area: 1 / 1 / 4 / 2;
+      background-color: red;
+    }
+
+    .live_conversation {
+      grid-area: 4 / 1 / 7 / 2;
+      background-color: tomato;
+    }
+    .live_hot {
+      grid-area: 7 / 1 / 13 / 2;
+      background-color: yellow;
+    }
+    .pair_value {
+      grid-area: 1 / 2 / 6 / 3;
+      background-color: azure;
+    }
+    .pair_value {
+      grid-area: 1 / 2 / 6 / 3;
+      background-color: azure;
+    }
+    .value_var {
+      grid-area: 6 / 2 / 13 / 3;
+      background-color: darkcyan;
+    }
+    .dome_price {
+      grid-area: 1 / 3 / 5 / 4;
+      background-color: aquamarine;
+    }
+    .compet_price {
+      grid-area: 5 / 3 / 8 / 4;
+      background-color: blue;
+    }
+    .compet_graph {
+      grid-area: 8 / 3 / 13 / 4;
+      background-color: gray;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title height={"2vh"}>
+        적정가격
+        <span> 모니터링 </span>
+        대시보드
+      </Title>
+      <div className="grid">
+        <div className="today_sell"></div>
+        <div className="live_conversation"></div>
+        <div className="live_hot"></div>
+        <div className="pair_value"></div>
+        <div className="value_var"></div>
+        <div className="dome_price"></div>
+        <div className="compet_price"></div>
+        <div className="compet_graph"></div>
+      </div>
+    </Wrapper>
   );
 }
 
---
+++ admin/src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  // color
+  DarkBlue: "#27304d",
+  MidBlue: "#363f5e",
+  LightBlue: "#6f7693",
+  White: "#f5f6fa",
+
+  // Font Size
+  LargeFont: "30px",
+  MediumFont: "24px",
+};
+
+export default theme;
