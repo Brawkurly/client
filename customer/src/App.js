669b1f513689fdffa219c69280f1bf89ac78c1a6
jihyeon <spy03128@naver.com>
2022-08-21 00:50:20 +0900

    [feat] Client 페이지 홈화면 디자인 (#4)

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,8 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./components/header/Navigation/Navigation";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
 function App() {
   return (
-    <div className="App">
-      <h1>customer page</h1>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
