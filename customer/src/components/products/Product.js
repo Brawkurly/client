81e5bbfed3460c6b57d3d82f3ac169282fd2a7e8
jihyeon <spy03128@naver.com>
2022-08-21 19:06:30 +0900

    [feat] 디테일 페이지 상품 연결 (#11)

--- customer/src/components/products/Product.js
+++ customer/src/components/products/Product.js
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./Product.module.css";
 
 function Product() {
+  const navigate = useNavigate();
   return (
     <>
-      <div className={styles.product}>
-        <Link to="/product/1">
-          <div className={styles.product_image}>
-            <img src="images/product/감귤1.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/1", {
+            state: {
+              productId: 1,
+              product: "감귤",
+              name: "당도선별 서귀포 감귤 1.5kg",
+              price: "16,800",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/감귤1.png" alt="product" />
+        </div>
+
         <div className={styles.store}>
           <span>감귤</span>
         </div>
@@ -23,31 +35,51 @@ function Product() {
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="/product/2">
-          <div className={styles.product_image}>
-            <img src="images/product/감귤2.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/2", {
+            state: {
+              productId: 2,
+              product: "감귤",
+              name: "[달콤트리] 고당도 제주 감귤 1.5kg",
+              price: "23,900",
+            },
+          });
+        }}
+      >
+        {" "}
+        <div className={styles.product_image}>
+          <img src="images/product/감귤2.png" alt="product" />
+        </div>
         <div className={styles.store}>
           <span>감귤</span>
         </div>
-
         <div className={styles.product_name}>
           <span>[달콤트리] 고당도 제주 감귤 1.5kg</span>
         </div>
-
         <div className={styles.product_price}>
           <span className={styles.price}>23,900</span>
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/고구마1.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/3", {
+            state: {
+              productId: 3,
+              product: "고구마",
+              name: "[KF365] 호박고구마 800g/봉",
+              price: "4,780",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/고구마1.png" alt="product" />
+        </div>
+
         <div className={styles.store}>
           <span>고구마</span>
         </div>
@@ -61,12 +93,23 @@ function Product() {
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/고구마2.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/4", {
+            state: {
+              productId: 4,
+              product: "고구마",
+              name: "[KF365] 밤고구마 800g/봉",
+              price: "5,580",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/고구마2.png" alt="product" />
+        </div>
+
         <div className={styles.store}>
           <span>고구마</span>
         </div>
@@ -80,31 +123,23 @@ function Product() {
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/쌀1.png" alt="product" />
-          </div>
-        </Link>
-        <div className={styles.store}>
-          <span>쌀</span>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/5", {
+            state: {
+              productId: 5,
+              product: "쌀",
+              name: "영광 신동진 쌀 10kg",
+              price: "29,900",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/쌀1.png" alt="product" />
         </div>
 
-        <div className={styles.product_name}>
-          <span>임금님표 이천쌀(추청) 10kg</span>
-        </div>
-
-        <div className={styles.product_price}>
-          <span className={styles.price}>31,900</span>
-          <span className={styles.unit}>원</span>
-        </div>
-      </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/쌀2.png" alt="product" />
-          </div>
-        </Link>
         <div className={styles.store}>
           <span>쌀</span>
         </div>
@@ -118,12 +153,54 @@ function Product() {
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/김치1.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/6", {
+            state: {
+              productId: 6,
+              product: "쌀",
+              name: "임금님표 이천쌀(추청) 10kg",
+              price: "31,900",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/쌀2.png" alt="product" />
+        </div>
+
+        <div className={styles.store}>
+          <span>쌀</span>
+        </div>
+
+        <div className={styles.product_name}>
+          <span>임금님표 이천쌀(추청) 10kg</span>
+        </div>
+
+        <div className={styles.product_price}>
+          <span className={styles.price}>31,900</span>
+          <span className={styles.unit}>원</span>
+        </div>
+      </div>
+
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/7", {
+            state: {
+              productId: 7,
+              product: "김치",
+              name: "[한국농협김치] 포기김치 2.5kg",
+              price: "22,000",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/김치1.png" alt="product" />
+        </div>
+
         <div className={styles.store}>
           <span>김치</span>
         </div>
@@ -137,12 +214,23 @@ function Product() {
           <span className={styles.unit}>원</span>
         </div>
       </div>
-      <div className={styles.product}>
-        <Link to="#">
-          <div className={styles.product_image}>
-            <img src="images/product/김치2.png" alt="product" />
-          </div>
-        </Link>
+      <div
+        className={styles.product}
+        onClick={() => {
+          navigate("/product/8", {
+            state: {
+              productId: 8,
+              product: "김치",
+              name: "[한국의집] 배추김치 2.5kg",
+              price: "30,000",
+            },
+          });
+        }}
+      >
+        <div className={styles.product_image}>
+          <img src="images/product/김치2.png" alt="product" />
+        </div>
+
         <div className={styles.store}>
           <span>김치</span>
         </div>
